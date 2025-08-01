3919012d32a7d9879b7f774e37a4f74cb81bebe5
CodeByAashik <thakuraashik98121@gmail.com>
2025-08-01 07:42:57 +0545

    fix the prepare statement of search bar operation

--- search.php
+++ search.php
@@ -57,8 +57,13 @@ include 'components/add_cart.php';
       <?php
          if(isset($_POST['search_box']) OR isset($_POST['search_btn'])){
          $search_box = $_POST['search_box'];
-         $select_products = $conn->prepare("SELECT * FROM `products` WHERE name LIKE '%{$search_box}%'");
-         $select_products->execute();
+         // $select_products = $conn->prepare("SELECT * FROM `products` WHERE name LIKE '%{$search_box}%'");
+         // $select_products->execute();
+
+         $select_products = $conn->prepare("SELECT * FROM `products` WHERE name LIKE ?");
+         $searchTerm = "%{$search_box}%";
+         $select_products->execute([$searchTerm]);
+
          if($select_products->rowCount() > 0){
             while($fetch_products = $select_products->fetch(PDO::FETCH_ASSOC)){
       ?>
