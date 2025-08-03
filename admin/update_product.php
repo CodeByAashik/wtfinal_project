80462b36e858f8995ad9f61c98c8eefd6221ef1c
CodeByAashik <thakuraashik98121@gmail.com>
2025-08-03 11:35:06 +0545

    fix the icon of admin panel

--- admin/update_product.php
+++ admin/update_product.php
@@ -49,7 +49,7 @@ if (isset($_POST['update'])) {
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>Update Product</title>
-   <link rel="icon" href="images/LYgjKqzpQb.ico" type="image/x-icon">
+   <link rel="icon" href="images/logo.png" type="image/x-icon">
 
    <!-- font awesome -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
